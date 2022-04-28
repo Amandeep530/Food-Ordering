2fb4290116d82fe851ad8e98d1975e7bbf4cdd2b
Amandeep530 <80556414+Amandeep530@users.noreply.github.com>
2022-04-28 21:24:32 +0530

    Update CheckOut.js

--- src/components/CheckOut.js
+++ src/components/CheckOut.js
@@ -6,7 +6,7 @@ function CheckOut() {
     return (
         <div className="checkOutOrder">
             <div className="d-flex align-items-center">
-            <img className="checkOutOrderImg img-responsive" src="https://assetcdn.pickupp.io/prime/8a01a8893881250a47cee80100a73ff5ac5bd4fe_Ramadan-Food-Delivery.jpg" alt="" />
+            <img className="checkOutOrderImg img-responsive" src="https://img.freepik.com/free-psd/delivery-service-banner-3d-illustration_1419-2395.jpg?t=st=1651161107~exp=1651161707~hmac=424f0214a56726906ca22f8f43a3658aa55279df8f5182537ef985437527b664&w=740" alt="" />
             <DeliveryInfo/>
             </div>
         </div>
